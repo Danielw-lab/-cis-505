eee1ab7e447d38b5e80b4518dfb25c26f171c616
Danielw-lab <81033377+Danielw-lab@users.noreply.github.com>
2024-03-14 13:27:32 -0500

    Update WoldegiorgisHelloWorld.java

--- Module__1/HelloWorldApp/WoldegiorgisHelloWorld.java
+++ Module__1/HelloWorldApp/WoldegiorgisHelloWorld.java
@@ -1,1 +1,13 @@
+// WoldegiorgisHelloWorld.java
+
+public class WoldegiorgisHelloWorld {
+    public static void main(String[] args) {
+        // Define first and last name variables
+        String firstName = "Daniel";
+        String lastName = "Woldegiorgis";
+        
+        // Print "Hello World from YourFirstName YourLastName" using printf
+        System.out.printf("Hello World from %s %s.%n", firstName, lastName);
+    }
+}
 
