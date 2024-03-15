49a40a76a9a11cff369cc0f95daed30f30285d2b
Danielw-lab <81033377+Danielw-lab@users.noreply.github.com>
2024-03-15 13:35:17 -0500

    Update WoldegiorgisHelloWorld.java

--- Module__1/HelloWorldApp/WoldegiorgisHelloWorld.java
+++ Module__1/HelloWorldApp/WoldegiorgisHelloWorld.java
@@ -1,8 +1,7 @@
 */ 
      Liang, Y.D. (2019). Introduction to Java Programming and Data Structures:
      Comprehensive Version (12th ed.). Pearson Education, Inc.
-     Modified by R. Krasso 2021
-     Additional modifications by A. Student 2021
+     Modified by Daniel Woldegiorgis 2024.    
  */
     
          
