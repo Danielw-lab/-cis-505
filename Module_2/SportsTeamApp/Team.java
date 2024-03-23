0a0c4fa279b4cce6f6a80407b802fae00ec9aa63
Danielw-lab <81033377+Danielw-lab@users.noreply.github.com>
2024-03-22 22:54:21 -0500

    Update Team.java

--- Module_2/SportsTeamApp/Team.java
+++ Module_2/SportsTeamApp/Team.java
@@ -1,3 +1,11 @@
+/*
+    Liang, Y.D. (2019). Introduction to Java Programming and Data Structures: 
+        Comprehensive Version (12th ed.). Pearson Education, Inc.
+   “Stackoverflow.Com.” Fan Prob with toString() Method -- Simple [Duplicate], Nov. 2016,
+   stackoverflow.com/questions/27006339/fan-prob-with-tostring-method-simple. 
+    Additional modifications by Daniel W. 2024
+*/ 
+
 import java.util.Scanner;
 
 public class Team {
