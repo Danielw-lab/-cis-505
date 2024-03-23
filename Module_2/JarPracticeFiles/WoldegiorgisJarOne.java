36d311b2303c530e738f08066635258e17b18408
Danielw-lab <81033377+Danielw-lab@users.noreply.github.com>
2024-03-23 14:45:48 -0500

    Delete Module_2/JarPracticeFiles directory

--- Module_2/JarPracticeFiles/WoldegiorgisJarOne.java
+++
@@ -1,7 +0,0 @@
-package Module_2.JarPracticeFiles;
-
-public class WoldegiorgisJarOne {
-    public static void main(String[] args) {
-        System.out.println("This is WoldegiorgisJarOne. ");
-    }
-}
--- Module_2/JarPracticeFiles/WoldegiorgisJarTwo.java
+++
@@ -1,6 +0,0 @@
-package Module_2.JarPracticeFiles;
-public class WoldegiorgisJarTwo {
-    public static void main(String[] args) {
-        System.out.println(" This is WoldegiorgisJarTwo.");
-    }
-}
