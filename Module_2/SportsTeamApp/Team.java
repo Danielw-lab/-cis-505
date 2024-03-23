c3ed39b5d9bf3c3d9eb8989fc12f9b7f11d51b7a
Danielw-lab <81033377+Danielw-lab@users.noreply.github.com>
2024-03-22 22:53:53 -0500

    Update Team.java

--- Module_2/SportsTeamApp/Team.java
+++ Module_2/SportsTeamApp/Team.java
@@ -1,1 +1,34 @@
+import java.util.Scanner;
 
+public class Team {
+    private String teamName;
+    private String[] players;
+    private int playerCount;
+
+    public Team(String teamName) {
+        this.teamName = teamName;
+        this.players = new String[20];
+        this.playerCount = 0;
+    }
+
+    public void addPlayer(String playerName) {
+        if (playerCount < players.length) {
+            players[playerCount] = playerName;
+            playerCount++;
+        } else {
+            System.out.println("Cannot add player. Team is full.");
+        }
+    }
+
+    public String[] getPlayers() {
+        return players;
+    }
+
+    public int getPlayerCount() {
+        return playerCount;
+    }
+
+    public String getTeamName() {
+        return teamName;
+    }
+}
