3ffa68a06df66f8027fc27c99fe609642529cd9f
Daniel T <dantek123@gmail.com>
2024-03-23 13:31:14 -0500

    WoldegiorgisCombined.jar

--- Module_2/JarPracticeFiles/WoldegiorgisJarOne.java
+++ Module_2/JarPracticeFiles/WoldegiorgisJarOne.java
@@ -2,6 +2,6 @@ package Module_2.JarPracticeFiles;
 
 public class WoldegiorgisJarOne {
     public static void main(String[] args) {
-        System.out.println("This is WoldegiorgisJarOne.");
+        System.out.println("This is WoldegiorgisJarOne. ");
     }
 }
