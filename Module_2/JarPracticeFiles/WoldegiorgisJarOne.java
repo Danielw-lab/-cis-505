f1ad8d6b4564e9e3ad6d485eb8881fb8ed7e8ea0
Daniel T <dantek123@gmail.com>
2024-03-23 15:02:39 -0500

    Assignemnt 2.2 Jar File

---
+++ Module_2/JarPracticeFiles/WoldegiorgisJarOne.java
@@ -0,0 +1,8 @@
+package Module_2.JarPracticeFiles;
+
+public class WoldegiorgisJarOne {
+
+    public static void main(String[] args) {
+        System.out.println("This is WoldegiorgisJarOne.");
+    }
+}
