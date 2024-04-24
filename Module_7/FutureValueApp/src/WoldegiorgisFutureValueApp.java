73d7425605abe57e37e12d9ffca7b8d0440f1ae8
Daniel T <dantek123@gmail.com>
2024-04-24 00:27:33 -0500

    Week 7

---
+++ Module_7/FutureValueApp/src/WoldegiorgisFutureValueApp.java
@@ -0,0 +1,14 @@
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class WoldegiorgisFutureValueApp extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        // Your code for the start method goes here
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
